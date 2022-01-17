48d62e938a22deea81aa1d28f4c34d98096d2ec8
Yuval Ben Amram <yuvalb99@hotmail.com>
2022-01-17 21:26:44 +0200

    Added fields to Pet model

--- app/src/main/java/com/example/purrfectmatch/model/Pet.java
+++ app/src/main/java/com/example/purrfectmatch/model/Pet.java
@@ -17,6 +17,9 @@ public class Pet {
     @NonNull
     String id = "";
     String name = "";
+    int age = 0;
+    String address = "";
+    String description = "";
     Long updateDate = new Long(0);
 
     public void setUpdateDate(Long updateDate) {
@@ -24,9 +27,12 @@ public class Pet {
     }
 
     public Pet(){}
-    public Pet(String name, String id) {
+    public Pet(String name, String id, int age, String address, String description) {
         this.name = name;
         this.id = id;
+        this.age = age;
+        this.address = address;
+        this.description = description;
     }
 
     public void setId(String id) {
@@ -37,18 +43,39 @@ public class Pet {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
+    public void setAge(int age) {
+        this.age = age;
     }
 
-    public String getId() {
-        return id;
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() { return name; }
+
+    public String getId() { return id; }
+
+    public int getAge() { return age; }
+
+    public String getAddress() { return address; }
+
+    public String getDescription() { return description; }
+
+    public Long getUpdateDate() {
+        return updateDate;
     }
 
     public Map<String, Object> toJson() {
         Map<String, Object> json = new HashMap<String, Object>();
         json.put("id",id);
         json.put("name",name);
+        json.put("age",age);
+        json.put("address",address);
+        json.put("description",description);
         json.put("updateDate", FieldValue.serverTimestamp());
         return json;
     }
@@ -56,16 +83,15 @@ public class Pet {
     public static Pet create(Map<String, Object> json) {
         String id = (String) json.get("id");
         String name = (String) json.get("name");
+        int age = (int) json.get("age");
+        String address = (String) json.get("address");
+        String description = (String) json.get("description");
         Timestamp ts = (Timestamp)json.get("updateDate");
         Long updateDate = ts.getSeconds();
 
-        Pet pet = new Pet(name,id);
+        Pet pet = new Pet(name,id,age,address,description);
         pet.setUpdateDate(updateDate);
         return pet;
-    }
-    //TODO:...
-    public Long getUpdateDate() {
-        return updateDate;
     }
 }
 
