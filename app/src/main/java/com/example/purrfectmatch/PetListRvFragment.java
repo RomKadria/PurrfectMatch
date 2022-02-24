ded821d26adcdc87bc69dfd91cdc2ca5da56f8a5
rotem <rzrotem@gmail.com>
2022-02-24 21:18:54 +0200

    WIP

---
+++ app/src/main/java/com/example/purrfectmatch/PetListRvFragment.java
@@ -0,0 +1,155 @@
+package com.example.purrfectmatch;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.purrfectmatch.model.Model;
+import com.example.purrfectmatch.model.Pet;
+
+import java.util.List;
+
+public class StudentListRvFragment extends Fragment {
+    PetListRvViewModel viewModel;
+    MyAdapter adapter;
+    SwipeRefreshLayout swipeRefresh;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        viewModel = new ViewModelProvider(this).get(PetListRvViewModel.class);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_pet_list,container,false);
+
+        swipeRefresh = view.findViewById(R.id.studentlist_swiperefresh);
+        swipeRefresh.setOnRefreshListener(() -> Model.instance.refreshPetList());
+
+        RecyclerView list = view.findViewById(R.id.studentlist_rv);
+        list.setHasFixedSize(true);
+
+        list.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        adapter = new MyAdapter();
+        list.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(View v,int position) {
+                String stId = viewModel.getData().getValue().get(position).getId();
+                Navigation.findNavController(v).navigate(PetListRvFragmentDirections.actionPetListRvFragmentToPetDetailsFragment(stId));
+
+            }
+        });
+
+        setHasOptionsMenu(true);
+        viewModel.getData().observe(getViewLifecycleOwner(), list1 -> refresh());
+        swipeRefresh.setRefreshing(Model.instance.getPetListLoadingState().getValue() == Model.PetListLoadingState.loading);
+        Model.instance.getPetListLoadingState().observe(getViewLifecycleOwner(), PetListLoadingState -> {
+            if (PetListLoadingState == Model.PetListLoadingState.loading){
+                swipeRefresh.setRefreshing(true);
+            }else{
+                swipeRefresh.setRefreshing(false);
+            }
+
+        });
+        return view;
+    }
+
+    private void refresh() {
+        adapter.notifyDataSetChanged();
+        swipeRefresh.setRefreshing(false);
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView nameTv;
+
+        public MyViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+            super(itemView);
+            nameTv = itemView.findViewById(R.id.listrow_name_tv);
+            idTv = itemView.findViewById(R.id.listrow_id_tv);
+            cb = itemView.findViewById(R.id.listrow_cb);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    listener.onItemClick(v,pos);
+                }
+            });
+        }
+    }
+
+    interface OnItemClickListener{
+        void onItemClick(View v,int position);
+    }
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
+
+        OnItemClickListener listener;
+        public void setOnItemClickListener(OnItemClickListener listener){
+            this.listener = listener;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.pet_list_row,parent,false);
+            MyViewHolder holder = new MyViewHolder(view,listener);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+            Pet pet = viewModel.getData().getValue().get(position);
+            holder.nameTv.setText(pet.getName());
+            holder.idTv.setText(pet.getId());
+            holder.cb.setChecked(pet.isFlag());
+        }
+
+        @Override
+        public int getItemCount() {
+            if(viewModel.getData().getValue() == null){
+                return 0;
+            }
+            return viewModel.getData().getValue().size();
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.student_list_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.addStudentFragment){
+            Log.d("TAG","ADD...");
+            return true;
+        }else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/purrfectmatch/PetListRvViewModel.java
@@ -0,0 +1,20 @@
+package com.example.purrfectmatch;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.purrfectmatch.model.Model;
+import com.example.purrfectmatch.model.Pet;
+
+import java.util.List;
+
+public class PetListRvViewModel extends ViewModel {
+    LiveData<List<Pet>> data;
+
+    public PetListRvViewModel(){
+        data = Model.instance.getAll();
+    }
+    public LiveData<List<Pet>> getData() {
+        return data;
+    }
+}
