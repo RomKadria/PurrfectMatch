635c5d3d916fde3167defc2f1ef7ff9d0cd1ed7b
rotem <rzrotem@gmail.com>
2022-03-04 15:01:15 +0200

    Rename

--- app/src/main/java/com/example/purrfectmatch/ChatFragment.java
+++ app/src/main/java/com/example/purrfectmatch/ChatFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Chat#newInstance} factory method to
+ * Use the {@link ChatFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Chat extends Fragment {
+public class ChatFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class Chat extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Chat() {
+    public ChatFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class Chat extends Fragment {
      * @return A new instance of fragment Chat.
      */
     // TODO: Rename and change types and number of parameters
-    public static Chat newInstance(String param1, String param2) {
-        Chat fragment = new Chat();
+    public static ChatFragment newInstance(String param1, String param2) {
+        ChatFragment fragment = new ChatFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
