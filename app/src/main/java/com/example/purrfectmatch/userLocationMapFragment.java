a62187c65ce556d95113eeb66df2c60022f40580
Eran <eran@nizry.com>
2022-03-11 19:25:22 +0200

    Get current location when granted permissions

--- app/src/main/java/com/example/purrfectmatch/userLocationMapFragment.java
+++ app/src/main/java/com/example/purrfectmatch/userLocationMapFragment.java
@@ -3,22 +3,31 @@ package com.example.purrfectmatch;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.List;
@@ -28,7 +37,14 @@ public class userLocationMapFragment extends Fragment {
     private GoogleMap mMap;
     SearchView searchView;
     View view;
+    Marker marker;
 
+    private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
+    private static final int DEFAULT_ZOOM = 15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean locationPermissionGranted;
+    private Location lastKnownLocation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -44,9 +60,21 @@ public class userLocationMapFragment extends Fragment {
         @Override
         public void onMapReady(GoogleMap googleMap) {
             mMap = googleMap;
-//            LatLng sydney = new LatLng(-34, 151);
-//            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+            // Turn on the My Location layer and the related control on the map.
+            if (mMap == null) {
+                return;
+            }
+            try {
+                if (!locationPermissionGranted) {
+                    getLocationPermission();
+                }
+            } catch (SecurityException e)  {
+                Log.e("Exception: %s", e.getMessage());
+            }
+
+            // Get the current location of the device and set the position of the map.
+            getDeviceLocation();
         }
     };
 
@@ -56,6 +84,9 @@ public class userLocationMapFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_user_location_map, container, false);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getContext());
+
         return view;
     }
 
@@ -88,11 +119,11 @@ public class userLocationMapFragment extends Fragment {
 
                         LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
 
-                        mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                        marker.setPosition(latLng);
 
                         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
                     } else {
-                        Toast.makeText(getContext(), "aaa", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), location + " doesn't exist", Toast.LENGTH_SHORT).show();
                     }
                 }
                 return false;
@@ -106,6 +137,78 @@ public class userLocationMapFragment extends Fragment {
 
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+        if (requestCode
+                == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+                getDeviceLocation();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+            getDeviceLocation();
+        } else {
+            requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this.getActivity(), new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            lastKnownLocation = task.getResult();
+                            if (lastKnownLocation != null) {
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(lastKnownLocation.getLatitude(),
+                                                lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                                LatLng latLng = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+                                MarkerOptions markerOptions = new MarkerOptions().position(latLng);
+                                marker = mMap.addMarker(markerOptions);
+                            }
+                        } else {
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            } else {
+                mMap.moveCamera(CameraUpdateFactory
+                        .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                MarkerOptions markerOptions = new MarkerOptions().position(defaultLocation);
+                marker = mMap.addMarker(markerOptions);
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage(), e);
         }
     }
 }
